9836a764b273d6b7d2f065d62cb92128f98f1b9e
onuproy <anupme01@gmail.com>
2021-10-03 18:59:08 +0600

    file upload all

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Button, Card, Col, Row, Spinner } from 'react-bootstrap';
+import { useEffect, useState } from 'react';
+import News from './components/News/News';
 
 function App() {
+  const [news, setNews] = useState([]);
+  useEffect(() => {
+    fetch('https://newsapi.org/v2/everything?q=tesla&from=2021-09-03&sortBy=publishedAt&apiKey=a015b0a244184ba3af40c8ad9c962459')
+      .then(res => res.json())
+      .then(data => setNews(data.articles))
+
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>To day all english news in world</h2>
+      {news.length === 0 ?
+      <Spinner animation="border" />
+      :
+        <Row xs={2} md={3} className="g-4">
+          {
+            news.map(nw => <News news={nw}></News>)
+          }
+        </Row>
+      }
+
+      {/* <Button variant="primary"> Green</Button>
+      <Spinner animation="border" role="status">
+        <span className="visually-hidden">Loading...</span>
+      </Spinner> */}
     </div>
   );
 }
---
+++ src/components/News/News.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './News.css'
+import { Card, Col } from 'react-bootstrap';
+
+const News = (props) => {
+
+    const {title, description, urlToImage} = props.news;
+
+    return (
+        <Col>
+            <Card>
+                <Card.Img variant="top" src={urlToImage} />
+                <Card.Body>
+                    <Card.Title>{title}</Card.Title>
+                    <Card.Text>
+                        {description}
+                    </Card.Text>
+                </Card.Body>
+            </Card>
+        </Col>
+    );
+};
+
+export default News;
